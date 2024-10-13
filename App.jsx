bac955dea299441402bd15876805421f5151425b
AishatulHani12 <d20221101861@m365.upsi.edu.my>
2024-10-14 01:34:56 +0800

    firstCommit

---
+++ App.jsx
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './Screens/LoginScreen';
+import RegisterScreen from './Screens/RegisterScreen'; 
+
+const Stack = createStackNavigator();
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHD-6qZtttt4ZHhVJrEdeFsCJTv4QTsYU",
+    authDomain: "signalclonetest1.firebaseapp.com",
+    projectId: "signalclonetest1",
+    storageBucket: "signalclonetest1.appspot.com",
+    messagingSenderId: "1084514490628",
+    appId: "1:1084514490628:web:bf219e43186114a85ed851"
+  };
